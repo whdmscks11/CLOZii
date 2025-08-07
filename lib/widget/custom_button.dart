1240b0320728ea78a32a85e9bc915f9021fc87f6
eunchan <whdmscks11@gmail.com>
2025-08-07 17:08:37 +0900

    feat: 커스텀 버튼 위젯(CustomButton) 구현
    - 공통 버튼 UI를 위한 CustomButton 위젯 생성
    - onTap 콜백을 nullable로 선언하여 조건에 따라 버튼 활성화/비활성화 가능하도록 설계
    - 활성화 상태에 따라 버튼 배경색 및 텍스트 색상 동적으로 변경

--- lib/widget/custom_button.dart
+++ lib/widget/custom_button.dart
@@ -4,10 +4,34 @@ class CustomButton extends StatelessWidget {
   const CustomButton({super.key, required this.text, required this.onTap});
 
   final String text;
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    bool isButtonEnabled = onTap != null ? true : false;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: double.infinity,
+          height: 40.0,
+          decoration: BoxDecoration(
+            color: isButtonEnabled ? Colors.amber[800] : Colors.grey[350],
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: isButtonEnabled ? Colors.white : Colors.grey[500],
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
